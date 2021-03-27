8a8faef5c8e822b4cfdac8795cc9b66e1801be9a
Tibisfly <vielmatorotibisay@gmail.com>
2021-03-27 17:13:44 +0000

    starting

--- exercises/99-computeAverageOfNumbers/app.js
+++ exercises/99-computeAverageOfNumbers/app.js
@@ -1,1 +1,17 @@
 // Write your function here
+
+function computeAverageOfNumbers(nums){
+    let sum = 0;
+    if (nums && nums.length > 0) {
+        for (let i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+    } else {
+        return sum;
+    }
+    return sum / nums.length;
+};
+
+var input = [1,2,3,4,5];
+var output = computeAverageOfNumbers(input);
+console.log(output);
